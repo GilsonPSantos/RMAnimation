67657bcd0e21e46f08a86e1f905d6b262349353a
Gilson Santos <gilsonsantosti@gmail.com>
2020-12-10 14:51:41 -0300

    [add] Implementacao dos testes unitários da HomeInteractor

---
+++ RMAnimation/RMAnimationTests/BaseTests/JsonName.swift
@@ -0,0 +1,17 @@
+//
+//  JsonName.swift
+//  RMAnimationTests
+//
+//  Created by Gilson Santos on 09/12/20.
+//  Copyright © 2020 Gilson Santos. All rights reserved.
+//
+
+import Foundation
+
+struct JsonName {
+    static let CHARACTER_LIST_SUCCESS = "character_list_success"
+    static let CHARACTER_SUCCESS_ONLY_ONE_ELEMENT_VALID = "character_success_only_one_valid_element_in_the_list"
+    static let CHARACTER_EMPTY_LIST = "character_empty_list"
+    static let CHARACTER_ERROR_JSON_STRUCTURE = "character_error_error_in_json_structure"
+    static let CHARACTER_ERROR_REQUIRED_FIELDS = "character_error_required_fields"
+}
